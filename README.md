c23856b950c11944726a6ff907f00e6e5b299f11
Shteyd <shteyd.dev@gmail.com>
2022-06-22 16:05:23 +0300

    add redux

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Added:
  - flutter_hooks
  - flutter_bloc
  - provider
+ - redux
---
+++ clicker_redux/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:clicker_redux/counter.dart';
+import 'package:clicker_redux/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Redux Clicker',
+      home: MyHomePage(counterStore),
+    );
+  }
+}
