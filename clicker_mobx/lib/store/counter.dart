5561b6b5155758132d559f697a948ed3d9dae4e1
Shteyd <shteyd.dev@gmail.com>
2022-07-02 16:10:46 +0300

    update mobx

--- clicker_mobx/lib/store/counter.dart
+++ clicker_mobx/lib/store/counter.dart
@@ -1,9 +1,8 @@
-// ignore_for_file: library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
 
 part 'counter.g.dart';
 
-class Counter = _Counter with _$Counter;
+class Counter extends _Counter with _$Counter {}
 
 abstract class _Counter with Store {
   @observable
