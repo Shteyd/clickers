9f0fe6d34ec6fdda24e299a0eb04f2c536c2e2b0
Shteyd <shteyd.dev@gmail.com>
2022-07-08 06:13:34 +0300

    remove cupertino

--- clicker_riverpod/lib/main.dart
+++ clicker_riverpod/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:clicker_riverpod/view.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
